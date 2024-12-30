db2f7bfbb90c04fa97092e5a6ea6cd558dfad3c0
Tom Williams <hello@tomw.design>
2024-12-30 12:17:38 +0000

    Update swiperjs.js

--- swiperjs.js
+++ swiperjs.js
@@ -1,21 +1,6 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+$(".slider-main").each(function (index) {
+  const swiper = new Swiper($(this).find(".swiper")[0], {
+    //
+    
+  });
 });
